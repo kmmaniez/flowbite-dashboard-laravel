7212dfa37f986bbb8607cfd76d0749e87fb50f14
kmmaniez <dzlabsid@gmail.com>
2023-11-14 07:54:01 +0700

    initial commit

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en" class="dark">
+  <head>
+    <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Get started with a free and open-source admin dashboard layout built with Tailwind CSS and Flowbite featuring charts, widgets, CRUD layouts, authentication pages, and more">
+<meta name="author" content="Themesberg">
+<meta name="generator" content="Hugo 0.120.4">
+
+<title>Tailwind CSS Admin Dashboard - Flowbite</title>
+
+<link rel="canonical" href="{{ asset('flowbite') }}/">
+
+{{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="{{ asset('flowbite') }}/app.css">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('flowbite') }}/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('flowbite') }}/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('flowbite') }}/favicon-16x16.png">
+<link rel="icon" type="image/png" href="{{ asset('flowbite') }}/favicon.ico">
+{{-- <link rel="manifest" href="{{ asset('flowbite') }}"> --}}
+<link rel="manifest" href="{{ asset('flowbite') }}/site.webmanifest">
+<link rel="mask-icon" href="{{ asset('flowbite') }}/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="theme-color" content="#ffffff">
+<!-- Twitter -->
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="@">
+<meta name="twitter:creator" content="@">
+<meta name="twitter:title" content="Tailwind CSS Admin Dashboard - Flowbite">
+<meta name="twitter:description" content="Get started with a free and open-source admin dashboard layout built with Tailwind CSS and Flowbite featuring charts, widgets, CRUD layouts, authentication pages, and more">
+<meta name="twitter:image" content="{{ asset('flowbite') }}/">
+
+<!-- Facebook -->
+<meta property="og:url" content="{{ asset('flowbite') }}/">
+<meta property="og:title" content="Tailwind CSS Admin Dashboard - Flowbite">
+<meta property="og:description" content="Get started with a free and open-source admin dashboard layout built with Tailwind CSS and Flowbite featuring charts, widgets, CRUD layouts, authentication pages, and more">
+<meta property="og:type" content="website">
+<meta property="og:image" content="{{ asset('flowbite') }}/images/og-image.png">
+<meta property="og:image:type" content="image/png">
+
+
+
+
+
+
+<script>
+    
+    if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark');
+    } else {
+        document.documentElement.classList.remove('dark')
+    }
+</script>
+  </head>
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/tes', function () {
+Route::get('/admin', function () {
     return view('tes');
 });
 
